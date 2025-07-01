729eb5c588c1ce49453aa83d3e303742e28bc010
Abigail Chelanagt <abigailchelah@gmail.com>
2025-07-01 10:01:41 +0300

    restful lab

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -17,11 +15,30 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = db.session.execute(db.select(Plant)).scalars().all()
+        return [plant.to_dict() for plant in plants], 200
+
+    def post(self):
+        data = request.get_json()
+        new_plant = Plant(
+            name=data.get("name"),
+            image=data.get("image"),
+            price=data.get("price")
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        return new_plant.to_dict(), 201
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = db.session.get(Plant, id)
+        if plant:
+            return plant.to_dict(), 200
+        return {"error": "Plant not found"}, 404
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
